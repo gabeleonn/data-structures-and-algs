77044c71866745c927e6b44596aa7d215040df18
Gabriel Leon <gabriel.leon@baritecnologia.com.br>
2021-07-14 16:10:02 -0300

    feat: add node class

---
+++ src/data-structures/node/node.ts
@@ -0,0 +1,11 @@
+export class Node {
+  public left: any;
+  public right: any;
+  public value: any;
+
+  constructor(value: any) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
